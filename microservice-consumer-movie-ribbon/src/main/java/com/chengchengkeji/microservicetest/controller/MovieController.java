c81e865a188953b5c3d878eba92cc1379328bd2f
codeke <codeke@qq.com>
2018-05-02 11:36:23 +0800

    commit

--- microservice-consumer-movie-ribbon/src/main/java/com/chengchengkeji/microservicetest/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/chengchengkeji/microservicetest/controller/MovieController.java
@@ -1,16 +1,15 @@
 package com.chengchengkeji.microservicetest.controller;
 
 import com.chengchengkeji.microservicetest.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 /**
  * @company: 甘肃诚诚网络技术有限公司
@@ -21,23 +20,25 @@ import java.util.List;
 @RestController
 public class MovieController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${user.userServiceUrl}")
-    private String userServiceUrl;
-
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        System.out.println("userServiceUrl = " + userServiceUrl);
-        return this.restTemplate.getForObject(userServiceUrl + "user/" + id, User.class);
+        return this.restTemplate.getForObject("http://microservice-provider-user/" + "user/" + id, User.class);
     }
 
-    @GetMapping("/user-instance")
-    public List<ServiceInstance> showInfo() {
-        return this.discoveryClient.getInstances("microservice-provider-user");
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+        // 打印当前选择的是哪个节点
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
+
+
 }

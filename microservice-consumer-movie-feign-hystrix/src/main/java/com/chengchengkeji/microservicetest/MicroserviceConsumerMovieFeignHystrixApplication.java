9a94610a05db4e52e4b28757bc725abf476ba02c
codeke <codeke@qq.com>
2018-05-04 10:02:51 +0800

    commit

--- microservice-consumer-movie-feign-hystrix/src/main/java/com/chengchengkeji/microservicetest/MicroserviceConsumerMovieFeignHystrixApplication.java
+++ microservice-consumer-movie-feign-hystrix/src/main/java/com/chengchengkeji/microservicetest/MicroserviceConsumerMovieFeignHystrixApplication.java
@@ -3,13 +3,11 @@ package com.chengchengkeji.microservicetest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
 public class MicroserviceConsumerMovieFeignHystrixApplication {
 
 	public static void main(String[] args) {
